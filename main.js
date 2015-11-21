c180277125a8fcc1106f3679860e2ff77ef492f8
Nelepa Sergey <nelepa1995@mail.ru>
2015-11-21 14:04:41 +0300

    add check for the winner, add click on cell

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
 window.addEventListener("load", function(){
 	var startButton = document.querySelector(".startGame");
 	var field = document.querySelector(".field");
-	var cells = document.querySelectorAll(".row>div");
-
+	var cells = document.querySelectorAll(".cell");
+	var message = document.querySelector(".windowMessage");
+	var nextMove = "x"
 	startButton.addEventListener("click", function startGame(){
 		for(var i = 0; i < cells.length; i++){
 			cells[i].classList.remove("o");
 			cells[i].classList.remove("x");
 		}
+		nextMove = "x"
+		message.innerHTML = "";
 	});
-});
+
+
+	field.addEventListener("click", function handleCellClick(e){
+		if(!e.target.classList.contains('cell')){
+			return ;
+		}
+
+		if(getWinner()){
+			return;
+		}
+		e.target.classList.add(nextMove);
+		if(nextMove === "x"){
+			nextMove = "o";
+		}
+		else{
+			nextMove = "x";
+		}
+		var winner = getWinner();
+		if(winner){
+			if (winner==="x"){
+				message.innerHTML = "Win X";
+			}else{
+				message.innerHTML = "Win O";
+			}
+
+		}
+	});
+}); 
